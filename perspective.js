b23ab56016d727be4d540a54703b3d4464786088
meteor admin <ayhan.yildiz@yahoo.com>
2016-01-04 12:46:50 -0500

    multiple style setter replaced with setAttribute

--- perspective.js
+++ perspective.js
@@ -1,27 +1,36 @@
 (function (el) {
-  'use strict';
+    'use strict';
     var perspectiveStart = 1024,
         perspectiveDirection = 'Y';//'X' or ''
-  
+
     window.onresize = function () {
-      var windowSize = window.innerWidth;
+        var windowSize = window.innerWidth;
         for (var i = 0; i < el.length; i++) {
             var currentEl = el[i],
                 wrapperEl = wrap(currentEl, document.createElement('div'));
-                wrapperEl.style.perspective = '400px';
-                wrapperEl.style.width = '200px';
-                currentEl.style.transition = '1s';
-                currentEl.style.transform = dynoRotate(windowSize);
+            // wrapperEl.style.perspective = '400px';
+            // wrapperEl.style.width = '200px';
+            // currentEl.style.transition = '1s';
+            // currentEl.style.transform = dynoRotate(windowSize);
+            wrapperEl.setAttribute("style","perspective : 400px; width : 200px;");
+            currentEl.setAttribute("style","transition : 1s; transform : "  + dynoRotate(windowSize) + "");
+
         }
     };
     function dynoRotate(windowSize) {
-        var degree = windowSize < perspectiveStart ? Math.round(Math.abs((windowSize - perspectiveStart) / 10)) : 0 ;      
+        var degree = windowSize < perspectiveStart ? Math.round(Math.abs((windowSize - perspectiveStart) / 10)) : 0 ;
+
         return 'rotate' + perspectiveDirection +'(' + degree + 'deg)';
     }
 
     function wrap(el, wrapper) {
         var wrapperEl = el.parentNode.insertBefore(wrapper, el);
-            wrapper.appendChild(el);
-            return wrapperEl;
+        wrapper.appendChild(el);
+        return wrapperEl;
     }
 })(document.getElementsByClassName('perspectiveMe'));
+
+
+
+
+
