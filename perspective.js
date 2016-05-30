d6c40950c85c7dee09599efcb267b2e7a97228b6
ayhanyildiz <ayhan.yildiz@yahoo.com>
2016-05-30 11:42:53 -0400

    xx

--- perspective.js
+++ perspective.js
@@ -1,5 +1,5 @@
 (function() {
-    'use strict';
+    'use strict'; // 
     var el = document.getElementsByClassName('perspectiveMe'),
         perspectiveStart = 1024,
         perspectiveDirection = 'Y';//'X' or ' '
