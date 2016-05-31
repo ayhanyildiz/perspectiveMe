5bef6bf17bd5ca1567c0aad5c35cd1e750c5bedf
ayhanyildiz <ayhan.yildiz@yahoo.com>
2016-05-31 00:11:50 -0400

    whoo

--- perspective.js
+++ perspective.js
@@ -1,5 +1,5 @@
-(function() {
-    'use strict'; //woo
+ (function() {
+    'use strict'; //woo whoo
     var el = document.getElementsByClassName('perspectiveMe'),
         perspectiveStart = 1024,
         perspectiveDirection = 'Y';//'X' or ' '
